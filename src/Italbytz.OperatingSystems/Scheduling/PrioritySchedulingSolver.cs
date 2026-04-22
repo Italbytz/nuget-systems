4251ee6bc0a87dd3deb2e95a3b2fc6520c6f0caf
nunkesser <robin.nunkesser@hshl.de>
2026-04-22 07:31:39 +0200

    chore(trace): update PrioritySchedulingSolver.cs

--- src/Italbytz.OperatingSystems/Scheduling/PrioritySchedulingSolver.cs
+++ src/Italbytz.OperatingSystems/Scheduling/PrioritySchedulingSolver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Italbytz.OperatingSystems.Abstractions;
 
 namespace Italbytz.OperatingSystems
@@ -14,15 +15,28 @@ namespace Italbytz.OperatingSystems
             var result = 0.0;
             var processes = parameters.Values.Length;
             var remainingProcesses = processes;
-            foreach (var priority in new string[] { "sehr hoch", "hoch", "mittel", "niedrig", "sehr niedrig" })
+            var steps = new List<string>();
+            var priorityLadder = new string[] { "sehr hoch", "hoch", "mittel", "niedrig", "sehr niedrig" };
+            steps.Add($"Priority ladder: {string.Join(" > ", priorityLadder)}.");
+            foreach (var priority in priorityLadder)
             {
                 var index = Array.FindIndex(parameters.Priorities, e => e == priority);
-                result += (double)remainingProcesses * (double)parameters.Values[index] / (double)processes;
+                if (index < 0)
+                {
+                    steps.Add($"Priority '{priority}' not present in input and is skipped.");
+                    continue;
+                }
+
+                var burst = parameters.Values[index];
+                var contribution = (double)remainingProcesses * (double)burst / (double)processes;
+                result += contribution;
+                steps.Add($"Priority '{priority}' maps to index {index} with burst={burst}; remaining={remainingProcesses} -> contribution {contribution:0.###}, running score {result:0.###}.");
                 remainingProcesses--;
             }
             return new SchedulingSolution()
             {
-                Time = result
+                Time = result,
+                Steps = steps
             };
         }
     }
