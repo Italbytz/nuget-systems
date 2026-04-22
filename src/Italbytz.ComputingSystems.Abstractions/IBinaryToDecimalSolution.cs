29055e06cc441a01b9bdcc6b7105e2daa6a7e7c3
nunkesser <robin.nunkesser@hshl.de>
2026-04-22 07:31:38 +0200

    chore(trace): update IBinaryToDecimalSolution.cs

--- src/Italbytz.ComputingSystems.Abstractions/IBinaryToDecimalSolution.cs
+++ src/Italbytz.ComputingSystems.Abstractions/IBinaryToDecimalSolution.cs
@@ -1,6 +1,8 @@
+using Italbytz.Systems.Abstractions;
+
 namespace Italbytz.ComputingSystems.Abstractions;
 
-public interface IBinaryToDecimalSolution
+public interface IBinaryToDecimalSolution : ITracedSolution
 {
     int Decimal { get; set; }
 }
