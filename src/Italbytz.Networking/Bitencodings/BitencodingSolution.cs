ddbba1c322a642f1c675075c0ba7d5d5c726a013
nunkesser <robin.nunkesser@hshl.de>
2026-04-22 07:31:38 +0200

    chore(trace): update BitencodingSolution.cs

--- src/Italbytz.Networking/Bitencodings/BitencodingSolution.cs
+++ src/Italbytz.Networking/Bitencodings/BitencodingSolution.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Italbytz.Networking.Abstractions;
 
 namespace Italbytz.Networking
@@ -8,5 +9,6 @@ namespace Italbytz.Networking
         public string[] NRZ { get; set; } = Array.Empty<string>();
         public string[] NRZI { get; set; } = Array.Empty<string>();
         public string[] MLT3 { get; set; } = Array.Empty<string>();
+        public List<string> Steps { get; set; } = new();
     }
 }
