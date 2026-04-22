33c299d23b35644b394157fa9f199f5e440d388c
nunkesser <robin.nunkesser@hshl.de>
2026-04-22 07:31:38 +0200

    chore(trace): update TwosComplementSolver.cs

--- src/Italbytz.ComputingSystems/TwosComplementSolver.cs
+++ src/Italbytz.ComputingSystems/TwosComplementSolver.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Italbytz.ComputingSystems.Abstractions;
 
 namespace Italbytz.ComputingSystems;
@@ -6,9 +8,21 @@ public class TwosComplementSolver : ITwosComplementSolver
 {
     public ITwosComplementSolution Solve(ITwosComplementParameters parameters)
     {
+        var input = parameters.PositiveBinary;
+        var inverted = (byte)~input;
+        var plusOne = (byte)(inverted + 1);
+
+        var steps = new List<string>
+        {
+            $"Input bits: {Convert.ToString(input, 2).PadLeft(8, '0')} ({input}).",
+            $"Invert bits: {Convert.ToString(inverted, 2).PadLeft(8, '0')}.",
+            $"Add one: {Convert.ToString(plusOne, 2).PadLeft(8, '0')} -> signed value {unchecked((sbyte)plusOne)}."
+        };
+
         var solution = new TwosComplementSolution
         {
-            ComplementBinary = (sbyte)(-1*parameters.PositiveBinary)
+            ComplementBinary = unchecked((sbyte)plusOne),
+            Steps = steps
         };
         return solution;
     }
