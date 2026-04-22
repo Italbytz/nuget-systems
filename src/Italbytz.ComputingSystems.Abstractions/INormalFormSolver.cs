a27b616d5dd2b25e38815d0772080527d72d07c5
nunkesser <robin.nunkesser@hshl.de>
2026-04-22 08:01:07 +0200

    feat(abstractions): add INormalFormSolver

---
+++ src/Italbytz.ComputingSystems.Abstractions/INormalFormSolver.cs
@@ -0,0 +1,7 @@
+using Italbytz.AI.Abstractions;
+
+namespace Italbytz.ComputingSystems.Abstractions;
+
+public interface INormalFormSolver : ISolver<INormalFormParameters, INormalFormSolution>
+{
+}
