98d79802565f382f1fc61cfc7e3ac4a9181e6a88
nunkesser <robin.nunkesser@hshl.de>
2026-04-22 07:31:39 +0200

    chore(trace): update ShortestJobFirstSolver.cs

--- src/Italbytz.OperatingSystems/Scheduling/ShortestJobFirstSolver.cs
+++ src/Italbytz.OperatingSystems/Scheduling/ShortestJobFirstSolver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Italbytz.OperatingSystems.Abstractions;
 
@@ -12,18 +13,23 @@ namespace Italbytz.OperatingSystems
 
         public ISchedulingSolution Solve(ISchedulingParameters parameters)
         {
-            var orderedValues = parameters.Values.OrderBy(s => s);
+            var orderedValues = parameters.Values.OrderBy(s => s).ToArray();
             var result = 0.0;
             var processes = parameters.Values.Length;
             var remainingProcesses = processes;
+            var steps = new List<string>();
+            steps.Add($"SJF order: {string.Join(", ", orderedValues)}.");
             foreach (var value in orderedValues)
             {
-                result += (double)remainingProcesses * (double)value / (double)processes;
+                var contribution = (double)remainingProcesses * (double)value / (double)processes;
+                result += contribution;
+                steps.Add($"Remaining={remainingProcesses}, burst={value} -> contribution {contribution:0.###}, running score {result:0.###}.");
                 remainingProcesses--;
             }
             return new SchedulingSolution()
             {
-                Time = result
+                Time = result,
+                Steps = steps
             };
         }
     }
