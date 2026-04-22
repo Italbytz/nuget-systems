fd8077c58a342349c776ead7f69b3aa68cf68a15
nunkesser <robin.nunkesser@hshl.de>
2026-04-22 07:31:39 +0200

    chore(trace): update RoundRobinSolver.cs

--- src/Italbytz.OperatingSystems/Scheduling/RoundRobinSolver.cs
+++ src/Italbytz.OperatingSystems/Scheduling/RoundRobinSolver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Italbytz.OperatingSystems.Abstractions;
 
@@ -12,20 +13,26 @@ namespace Italbytz.OperatingSystems
 
         public ISchedulingSolution Solve(ISchedulingParameters parameters)
         {
-            var orderedValues = parameters.Values.OrderBy(s => s);
+            var orderedValues = parameters.Values.OrderBy(s => s).ToArray();
             var result = 0.0;
             var processes = parameters.Values.Length;
             var remainingProcesses = processes;
             var sharedTime = 0;
+            var steps = new List<string>();
+            steps.Add($"RoundRobin score uses ascending burst phases: {string.Join(", ", orderedValues)}.");
             foreach (var value in orderedValues)
             {
-                result += (double)remainingProcesses * ((double)value - (double)sharedTime) / (double)processes * (double)remainingProcesses;
+                var delta = (double)value - (double)sharedTime;
+                var contribution = (double)remainingProcesses * delta / (double)processes * (double)remainingProcesses;
+                result += contribution;
+                steps.Add($"remaining={remainingProcesses}, phaseDelta={delta:0.###} -> contribution {contribution:0.###}, running score {result:0.###}.");
                 sharedTime = value;
                 remainingProcesses--;
             }
             return new SchedulingSolution()
             {
-                Time = result
+                Time = result,
+                Steps = steps
             };
         }
     }
