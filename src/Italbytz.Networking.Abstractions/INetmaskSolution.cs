95c6a0eb474b5e9031d251ddb1bf526ddfd9a138
nunkesser <robin.nunkesser@hshl.de>
2026-04-22 07:31:38 +0200

    chore(trace): update INetmaskSolution.cs

--- src/Italbytz.Networking.Abstractions/INetmaskSolution.cs
+++ src/Italbytz.Networking.Abstractions/INetmaskSolution.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Net;
+using Italbytz.Systems.Abstractions;
 
 namespace Italbytz.Networking.Abstractions
 {
-    public interface INetmaskSolution
+    public interface INetmaskSolution : ITracedSolution
     {
         IPAddress NetworkAddress { get; set; }
         IPAddress HostAddress { get; set; }
