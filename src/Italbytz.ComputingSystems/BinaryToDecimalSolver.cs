d0c652233e8b78843a3a26ca013095434e5b693c
nunkesser <robin.nunkesser@hshl.de>
2026-04-22 07:31:38 +0200

    chore(trace): update BinaryToDecimalSolver.cs

--- src/Italbytz.ComputingSystems/BinaryToDecimalSolver.cs
+++ src/Italbytz.ComputingSystems/BinaryToDecimalSolver.cs
@@ -1,15 +1,27 @@
+using System.Collections.Generic;
 using Italbytz.ComputingSystems.Abstractions;
 
 namespace Italbytz.ComputingSystems;
 
 public class BinaryToDecimalSolver : IBinaryToDecimalSolver
 {
-    // Doing nothing, because for now it is possible to solve the problem in the transfer to the ViewModel
     public IBinaryToDecimalSolution Solve(IBinaryToDecimalParameters parameters)
     {
+        var steps = new List<string>();
+        var decimalValue = 0;
+
+        for (var bit = 0; bit < 8; bit++)
+        {
+            var bitValue = (parameters.Binary >> bit) & 0x1;
+            var contribution = bitValue == 1 ? (1 << bit) : 0;
+            decimalValue += contribution;
+            steps.Add($"Bit {bit} is {bitValue} -> contribution {contribution}, running sum {decimalValue}.");
+        }
+
         var solution = new BinaryToDecimalSolution
         {
-            Decimal = parameters.Binary
+            Decimal = decimalValue,
+            Steps = steps
         };
         return solution;
     }
