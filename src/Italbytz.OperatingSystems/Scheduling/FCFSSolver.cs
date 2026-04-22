e53e851d71e09ee559d2451a8ae82f6ef573b311
nunkesser <robin.nunkesser@hshl.de>
2026-04-22 07:31:39 +0200

    chore(trace): update FCFSSolver.cs

--- src/Italbytz.OperatingSystems/Scheduling/FCFSSolver.cs
+++ src/Italbytz.OperatingSystems/Scheduling/FCFSSolver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Italbytz.OperatingSystems.Abstractions;
 
 namespace Italbytz.OperatingSystems
@@ -14,14 +15,24 @@ namespace Italbytz.OperatingSystems
             var result = 0.0;
             var processes = parameters.Values.Length;
             var remainingProcesses = processes;
+            var steps = new List<string>();
+            steps.Add("FCFS uses input order without reordering.");
+            for (var index = 0; index < parameters.Values.Length; index++)
+            {
+                steps.Add($"Queue position {index + 1}: burst={parameters.Values[index]}.");
+            }
+
             foreach (var value in parameters.Values)
             {
-                result += (double)remainingProcesses * (double)value / (double)processes;
+                var contribution = (double)remainingProcesses * (double)value / (double)processes;
+                result += contribution;
+                steps.Add($"Remaining={remainingProcesses}, burst={value} -> contribution {contribution:0.###}, running score {result:0.###}.");
                 remainingProcesses--;
             }
             return new SchedulingSolution()
             {
-                Time = result
+                Time = result,
+                Steps = steps
             };
         }
     }
