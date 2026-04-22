9ee1a58a3920441c0de9e7bb8a11bf3ce1052375
nunkesser <robin.nunkesser@hshl.de>
2026-04-22 07:31:39 +0200

    chore(trace): update Test1.cs

--- tests/Italbytz.Systems.Tests/Test1.cs
+++ tests/Italbytz.Systems.Tests/Test1.cs
@@ -1,9 +1,11 @@
-﻿using Italbytz.ComputingSystems;
+﻿using System.Linq;
+using Italbytz.ComputingSystems;
 using Italbytz.ComputingSystems.Abstractions;
 using Italbytz.Networking;
 using Italbytz.Networking.Abstractions;
 using Italbytz.OperatingSystems;
 using Italbytz.OperatingSystems.Abstractions;
+using Italbytz.Systems.Abstractions;
 
 namespace Italbytz.Systems.Tests;
 
@@ -28,6 +30,54 @@ public sealed class ComputingSystemsTests
         var solution = solver.Solve(new TwosComplementParameters(5));
 
         Assert.AreEqual((sbyte)-5, solution.ComplementBinary);
+    }
+
+    [TestMethod]
+    public void Binary_addition_solver_exposes_bitwise_trace()
+    {
+        IBinaryAdditionSolver solver = new BinaryAdditionSolver();
+
+        var solution = solver.Solve(new BinaryAdditionParameters(255, 5));
+
+        Assert.AreEqual((ushort)260, solution.Sum);
+        Assert.IsGreaterThanOrEqualTo(8, solution.Steps.Count);
+        StringAssert.Contains(solution.Steps[0], "Bit 0");
+    }
+
+    [TestMethod]
+    public void Decimal_to_binary_solver_converts_via_division_trace()
+    {
+        IDecimalToBinarySolver solver = new DecimalToBinarySolver();
+
+        var solution = solver.Solve(new DecimalToBinaryParameters(42));
+
+        Assert.AreEqual(101010, solution.Binary);
+        Assert.IsNotEmpty(solution.Steps);
+        StringAssert.Contains(solution.Steps[0], "/ 2");
+    }
+
+    [TestMethod]
+    public void Binary_to_decimal_solver_converts_via_weighted_sum_trace()
+    {
+        IBinaryToDecimalSolver solver = new BinaryToDecimalSolver();
+
+        var solution = solver.Solve(new BinaryToDecimalParameters(45));
+
+        Assert.AreEqual(45, solution.Decimal);
+        Assert.HasCount(8, solution.Steps);
+        StringAssert.Contains(solution.Steps[0], "Bit 0");
+    }
+
+    [TestMethod]
+    public void Twos_complement_solver_exposes_invert_and_increment_trace()
+    {
+        ITwosComplementSolver solver = new TwosComplementSolver();
+
+        var solution = solver.Solve(new TwosComplementParameters(5));
+
+        Assert.AreEqual((sbyte)-5, solution.ComplementBinary);
+        Assert.HasCount(3, solution.Steps);
+        StringAssert.Contains(solution.Steps[1], "Invert bits");
     }
 
     [TestMethod]
@@ -77,6 +127,41 @@ public sealed class ComputingSystemsTests
     }
 
     [TestMethod]
+    public void Scheduling_solvers_expose_trace_steps_via_shared_contract()
+    {
+        ISchedulingSolver solver = new FCFSSolver();
+
+        var solution = solver.Solve(new SchedulingParameters(
+            [6, 2, 8, 3, 5],
+            ["mittel", "sehr hoch", "niedrig", "hoch", "sehr niedrig"]));
+
+        Assert.IsTrue(solution is ITracedSolution);
+        Assert.IsNotEmpty(solution.Steps);
+    }
+
+    [TestMethod]
+    public void Realtime_scheduling_solvers_expose_trace_steps_via_shared_contract()
+    {
+        IRealtimeSchedulingSolver rmsSolver = new RMSSolver();
+        IRealtimeSchedulingSolver edfSolver = new EDFSolver();
+
+        var parameters = new RealtimeSchedulingParameters
+        {
+            Requests = [(1, 4), (2, 5), (4, 10)]
+        };
+
+        var rmsSolution = rmsSolver.Solve(parameters);
+        var edfSolution = edfSolver.Solve(parameters);
+
+        Assert.IsTrue(rmsSolution is ITracedSolution);
+        Assert.IsTrue(edfSolution is ITracedSolution);
+        Assert.IsNotEmpty(rmsSolution.Steps);
+        Assert.IsNotEmpty(edfSolution.Steps);
+        Assert.IsGreaterThanOrEqualTo(32, rmsSolution.Processes.Length);
+        Assert.IsGreaterThanOrEqualTo(32, edfSolution.Processes.Length);
+    }
+
+    [TestMethod]
     public void Fifo_page_replacement_solver_counts_page_faults()
     {
         IPageReplacementSolver solver = new FIFOSolver();
@@ -102,5 +187,38 @@ public sealed class ComputingSystemsTests
         });
 
         Assert.IsNotEmpty(solution.History);
+    }
+
+    [TestMethod]
+    public void Bitencoding_solver_exposes_trace_steps()
+    {
+        IBitencodingSolver solver = new BitencodingSolver();
+
+        var solution = solver.Solve(new BitencodingParameters([1, 0, 1, 1, 0]));
+
+        Assert.IsNotEmpty(solution.Steps);
+        StringAssert.Contains(solution.Steps[0], "Input bits");
+    }
+
+    [TestMethod]
+    public void Crc_solver_exposes_trace_steps()
+    {
+        ICRCSolver solver = new CRCSolver();
+
+        var solution = solver.Solve(new CRCParameters(42));
+
+        Assert.IsNotEmpty(solution.Steps);
+        Assert.IsTrue(solution.Steps.Any(step => step.Contains("Generator polynomial")));
+    }
+
+    [TestMethod]
+    public void Netmask_solver_exposes_trace_steps()
+    {
+        INetmaskSolver solver = new NetmaskSolver();
+
+        var solution = solver.Solve(new NetmaskParameters("192.168.10.42", 24));
+
+        Assert.IsNotEmpty(solution.Steps);
+        Assert.IsTrue(solution.Steps.Any(step => step.Contains("Subnet mask")));
     }
 }
