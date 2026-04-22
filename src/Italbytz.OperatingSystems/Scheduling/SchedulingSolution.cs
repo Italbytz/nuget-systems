4128052b9cb27a1739d730b302b27e4e32ca0bc6
nunkesser <robin.nunkesser@hshl.de>
2026-04-22 07:31:39 +0200

    chore(trace): update SchedulingSolution.cs

--- src/Italbytz.OperatingSystems/Scheduling/SchedulingSolution.cs
+++ src/Italbytz.OperatingSystems/Scheduling/SchedulingSolution.cs
@@ -1,9 +1,11 @@
-﻿using Italbytz.OperatingSystems.Abstractions;
+﻿using System.Collections.Generic;
+using Italbytz.OperatingSystems.Abstractions;
 
 namespace Italbytz.OperatingSystems
 {
     public class SchedulingSolution : ISchedulingSolution
     {
         public double Time { get; set; }
+        public List<string> Steps { get; set; } = new();
     }
 }
